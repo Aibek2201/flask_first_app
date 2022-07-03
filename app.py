78c2a8c614633047144351001053c35500cd60c7
Aibek2201 <a.almabekuly@aues.kz>
2022-07-04 02:44:27 +0600

    2

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from urllib import request
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
+    if request.method == "POST":
+        return request.form['password']
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
-
