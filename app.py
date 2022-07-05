56de4526fec803025026741cbb409983694ca6a6
--global <a.almabekuly@aues.kz>
2022-07-06 03:00:06 +0600

    Flash message in browser.Termins:flash,import secrets,secrets.token_urlsafe(32),with_categories in html

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, flash
 from flask_mysqldb import MySQL
 from flask_bootstrap import Bootstrap
-import yaml
+import yaml, secrets
 
 app = Flask(__name__)
 Bootstrap(app)
+secret = secrets.token_urlsafe(32)
 
 #DB config
 db = yaml.load(open('db.yaml'))
@@ -13,18 +14,22 @@ app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.secret_key = secret
 mysql = MySQL(app)
 
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        form = request.form
-        name = form['name']
-        age = form['age']
-        cursor = mysql.connection.cursor()
-        name = generate_password_hash(name)
-        cursor.execute("INSERT INTO employee(name, age) VALUES(%s, %s)",(name,age))
-        mysql.connection.commit()
+        try:
+            form = request.form
+            name = form['name']
+            age = form['age']
+            cursor = mysql.connection.cursor()
+            cursor.execute("INSERT INTO employee(name, age) VALUES(%s, %s)",(name,age))
+            mysql.connection.commit()
+            flash("You data is succesfully added", "success")
+        except:
+            flash("The inserting is failed!", "danger")
     return render_template('index.html')
 
 @app.route('/employees')
